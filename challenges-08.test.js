8967f898f93852c6399cd1b7d4d3eb6e2e10fd59
Randee Orion <randeegood@gmail.com>
2020-01-27 17:48:54 -0800

    two passes

--- challenges-08.test.js
+++ challenges-08.test.js
@@ -30,7 +30,8 @@ For example, filterStringsWithVowels('gregor','hound','xyz') returns ['gregor', 
 
 
 const filterStringsWithVowels = (arr) => {
-  // Solution code here...
+  let regex = (/[aeiou]/gm);
+  return arr.filter( str => str.match(regex));
 };
 
 
